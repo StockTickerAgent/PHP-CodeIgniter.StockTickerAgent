0957cc930b4ed2ba257aecc86b5e13a929a0f9cf
Jan Ycasas <ycasas.jan@gmail.com>
2016-02-09 17:14:54 -0800

    Added Portfolio Stuff

--- application/controllers/Portfolio.php
+++ application/controllers/Portfolio.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Portfolio extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,16 +20,14 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
-        /*
-        $this->load->database();
-        $query = $this->db->query('SELECT Player FROM players');
-
-        foreach ($query->result() as $row) {
-            echo $row->Player . "";
-        }
-        */
+       
         
-        
+        $this->load->view("TempPortfolio");
 	}
+    
+    public function getPortfolio($person){
+        $this->load->model("PortfolioModel");
+        $data['query'] = $this->PortfolioModel->getResult($person);
+        $this->load->view("TempPortfolio",$data);
+    }
 }
