60acdd70a3d5002dec469885a84f3e82787b5768
Jan Ycasas <ycasas.jan@gmail.com>
2016-02-12 09:24:58 -0800

    Pass Current Holdings of the user, Pass the list of players

--- application/views/Portfolio_Single.php
+++ application/views/Portfolio_Single.php
@@ -1,3 +1,4 @@
+<h1>Recent Transactions</h1>
 <table>
   <thead>
     <td>Name</td>
@@ -14,3 +15,22 @@
     </tr>
   {/stocks}
 </table>
+
+<h1>Current Holdings</h1>
+<table>
+  <thead>
+    <td>Name</td>
+    <td>Value</td>
+    <td>Type</td>
+    <td>Quantity</td>
+  </thead>
+  {currentHoldings}
+    <tr>
+      <td>{Stock}</td>
+      <td>{Cash}</td>
+      <td>{Trans}</td>
+      <td>{Quantity}</td>
+    </tr>
+    {/currentHoldings}
+</table>
+
