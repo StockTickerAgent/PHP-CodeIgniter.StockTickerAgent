e408ac336e3cd57c115ac80d47bc587f32aa82cb
Anthony Smith <anthony@smith.lt>
2016-02-12 15:48:41 -0800

    Include links to other pages in header

--- application/views/Portfolio_Single.php
+++ application/views/Portfolio_Single.php
@@ -1,44 +1,46 @@
-<div class="row">
-  <div class="col-md-12">
-    <h2>Recent Transactions</h2>
-    <table>
-      <thead>
-        <td>Name</td>
-        <td>Value</td>
-        <td>Type</td>
-        <td>Quantity</td>
-      </thead>
-      {stocks}
-        <tr>
-          <td>{Stock}</td>
-          <td>{Cash}</td>
-          <td>{Trans}</td>
-          <td>{Quantity}</td>
-        </tr>
-      {/stocks}
-    </table>
+<div id="portfolio">
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Recent Transactions</h2>
+      <table>
+        <thead>
+          <td>Name</td>
+          <td>Value</td>
+          <td>Type</td>
+          <td>Quantity</td>
+        </thead>
+        {stocks}
+          <tr>
+            <td>{Stock}</td>
+            <td>{Cash}</td>
+            <td>{Trans}</td>
+            <td>{Quantity}</td>
+          </tr>
+        {/stocks}
+      </table>
+    </div>
   </div>
-</div>
 
-<div class="row">
-  <div class="col-md-12">
-    <h2>Current Holdings</h2>
-    <table>
-      <thead>
-        <td>Name</td>
-        <td>Value</td>
-        <td>Type</td>
-        <td>Quantity</td>
-      </thead>
-      {currentHoldings}
-        <tr>
-          <td>{Stock}</td>
-          <td>{Cash}</td>
-          <td>{Trans}</td>
-          <td>{Quantity}</td>
-        </tr>
-        {/currentHoldings}
-    </table>
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Current Holdings</h2>
+      <table>
+        <thead>
+          <td>Name</td>
+          <td>Value</td>
+          <td>Type</td>
+          <td>Quantity</td>
+        </thead>
+        {currentHoldings}
+          <tr>
+            <td>{Stock}</td>
+            <td>{Cash}</td>
+            <td>{Trans}</td>
+            <td>{Quantity}</td>
+          </tr>
+          {/currentHoldings}
+      </table>
+    </div>
   </div>
 </div>
 
--- application/views/base/_template.php
+++ application/views/base/_template.php
@@ -8,8 +8,9 @@ if (!defined('APPPATH'))
 <head>
     <title>{pagetitle}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
 <body>
   <div class="container">
@@ -17,7 +18,7 @@ if (!defined('APPPATH'))
       {header}
     </header>
 
-    <section>
+    <section id="content">
       {content}
     </section>
 
