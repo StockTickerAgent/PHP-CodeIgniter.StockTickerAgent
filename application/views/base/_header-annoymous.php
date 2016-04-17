175fc60f17fe690ffd36267c17ca3f22a0f6d6d0
Anthony Smith <anthony@smith.cm>
2016-04-16 17:31:52 -0700

    Move login and register links to right of the header

--- application/views/base/_header-annoymous.php
+++ application/views/base/_header-annoymous.php
@@ -8,24 +8,16 @@
   <li class="<?php echo active_link('Portfolio'); ?>">
     <a href="/portfolio">Portfolio</a>
   </li>
-  <li class="<?php echo active_link('Register'); ?>">
-    <a href="/register">Register</a>
-  </li>
-  <li class="<?php echo active_link('Log In'); ?>">
-    <a href="/login">Log In</a>
-  </li>
 </ul>
-<!--
+
 <div class="row">
-  <form method="POST" action="/login" class="navbar-form navbar-right" role="login">
-    <div class="form-group login-input col-xs-10 col-sm-7 col-md-7">
-      <select name="playername" class="form-control pull-right">
-        {playerList}
-        <option value="{Player}">{Player}</option>
-        {/playerList}
-      </select>
-    </div>
-    <input type="submit" value="Login" class="btn btn-default col-xs-2 col-sm-5 col-md-5 pull-right">
-  </form>
+  <div class="nav navbar-nav navbar-right">
+    <li class="<?php echo active_link('Register'); ?>">
+      <a href="/register">Register</a>
+    </li>
+    <li class="<?php echo active_link('Login'); ?>">
+      <a href="/login">Log In</a>
+    </li>
+  </div>
 </div>
--->
+
